f62411541bac0dfe844b45ac068c5a6f9164366b
Sean <jesky.sean@outlook.com>
2017-11-29 08:49:45 -0700

    Update fizz_buzz.cpp

--- fizz_buzz.cpp
+++ fizz_buzz.cpp
@@ -14,27 +14,29 @@ ALL IN ALL, pretty damn good I think for only coding for 4 months in C++
 #include "inclds"
 int main() 
 {
-    //I decided not to write this with unreasonable complexity:
-    
+    //I decided not to write this with unreasonable complexity:  
     //change values as desired: 
     unsigned var1 = 3;
     unsigned var2 = 5;
 
     for (int i = 1; i != 101; ++i) 
     {
-        //If the number matches only its self
-        if (i % var1 == 0 && i % var2 != 0) {
+     //If the number matches only its self
+        if (i % var1 == 0 && i % var2 != 0) 
+        {
             cout << i << "\t" << "fizz" << endl;
-        }            
-        //If the number matches only its self
-        else if (i % var1 != 0 && i % var2 == 0) {
-            cout << i << "\t" << "buzz" << endl;
-        }            
-        //If both numbers are of equal value: 
-        else if (i % var1 == 0 && i % var2 == 0) {
-            cout << i << " \t" << "fizzbuzz" << endl;
         }           
-        //execute code as normally would:
+     //If the number matches only its self
+        else if (i % var1 != 0 && i % var2 == 0) 
+        {
+            cout << i << "\t" << "buzz" << endl;
+        }           
+     //If both numbers are of equal value: 
+        else if (i % var1 == 0 && i % var2 == 0) 
+        {
+            cout << i << " \t" << "fizzbuzz" << endl;
+        }              
+     //execute code as normally would:
         else
         {
             cout << i << "\t" << i << endl;
